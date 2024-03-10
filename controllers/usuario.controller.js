0cf21ebe5e7e571acd0331e10fcfb8e6b46671b0
Moitas500 <camilofour1@gmail.com>
2024-03-10 14:04:09 -0500

    register validations

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -1,3 +1,5 @@
+import { check, validationResult } from 'express-validator'
+import Usuario from '../models/Usuario.js'
 
 const formLogin = ( req, res ) => {
     res.render('auth/login', {
@@ -11,6 +13,35 @@ const formRegister = ( req, res ) => {
     })
 }
 
+const register = async ( req, res ) => {
+    //Validation
+    await check( 'nombre' ).notEmpty().withMessage( 'Nombre obligatorio' ).run( req )
+    await check( 'email' ).isEmail().withMessage( 'No es un email' ).run( req )
+    await check( 'password' ).isLength({min: 6}).withMessage( 'El password debe ser de 6 caracteres' ).run( req )
+    await check( 'repetir_password' ).equals( 'password' ).withMessage( 'Los passwords no son iguales' ).run( req )
+
+    let result = validationResult( req )
+
+    //Verified result
+    if ( !result.isEmpty() ) {
+        return res.render('auth/register', {
+            pagina: 'Crear cuenta',
+            errores: result.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email
+            }
+        })
+    }
+
+    res.json( result.array() )
+
+    const usuario = await Usuario.create( req.body )
+
+    res.json(usuario)
+
+}
+
 const formForgetPassword = ( req, res ) => {
     res.render('auth/forget-password', {
         pagina: 'Recupera tu acceso a bienes raices'
@@ -20,5 +51,6 @@ const formForgetPassword = ( req, res ) => {
 export  {
     formLogin,
     formRegister,
-    formForgetPassword
+    formForgetPassword,
+    register
 }
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { formLogin, formRegister, formForgetPassword } from '../controllers/usuario.controller.js' 
+import { formLogin, formRegister, formForgetPassword, register } from '../controllers/usuario.controller.js' 
 
 const router = express.Router()
 
 router.get('/login', formLogin)
+
 router.get('/register', formRegister)
+router.post('/register', register)
+
 router.get('/forget-password', formForgetPassword)
 
 export default router
